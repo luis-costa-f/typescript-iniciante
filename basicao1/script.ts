aa1607732a0f322c220cfc53ab865d33d5b81669
Luis Costa <luiscosta.uva@gmail.com>
2023-10-26 18:33:48 -0300

    add: basicao de typescript

--- basicao1/script.ts
+++ basicao1/script.ts
@@ -1,23 +1,39 @@
-"use strict";
 let produto = "livro";
 let preco = 200;
-const carro = {
+
+const carro: {
+    marca: string;
+    portas: number;
+} = {
     marca: "audi",
     portas: 5,
 };
+
 const barato = preco < 400 ? true : "produto caro";
-function somar(a, b) {
+
+
+function somar (a: number, b: number): number {
     return a + b;
 }
+
 somar(4, 5);
+
 // somar(4, "4");
+
 const nintendo = {
     nome: "Nintendo",
     preco: "2000"
-};
-function transformarPreco(produto) {
+}
+
+function transformarPreco (produto: {nome: string; preco: string}) {
     produto.preco = "R$ " + produto.preco;
     return produto;
 }
-const produtoNovo = transformarPreco(nintendo);
-console.log(produtoNovo);
+
+const produtoNovo = transformarPreco(nintendo)
+
+console.log(produtoNovo)
+
+
+
+
---
+++ basicao1/script1.js
@@ -0,0 +1,28 @@
+//@ts-check
+
+const frase = 'Front End';
+const total = 100.0;
+const empresas = ['apple', 'microsoft'];
+
+frase.toLowerCase();
+//total.toLowerCase();
+
+const t = total.toFixed();
+
+console.log(t + 10);
+
+empresas.map((empresa) => console.log(empresa.toLowerCase()));
+
+const body = document.body;
+
+body.style.background = '#000';
+
+const button = document.querySelector('button');
+
+if (button) button.click();
+
+//const operacao = 100 + 200;
+//const operacao = 100 + true;
+const operacao = 100 + '300';
+
+console.log(operacao);
